34c052d35f8510365a89b320c9558b8ea9af0246
melodiessim98 <melodiessim98@gmail.com>
2019-07-08 12:50:03 +0800

    Add simple apollo client and provider

---
+++ start/client/src/index.js
@@ -0,0 +1,45 @@
+import { ApolloProvider } from 'react-apollo'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Pages from './pages'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+// import gql from 'graphql-tag'
+
+const cache = new InMemoryCache()
+const link = new HttpLink({
+	uri: 'http://localhost:4000/',
+})
+
+// instantiate client object
+const client = new ApolloClient({
+	cache,
+	link,
+})
+
+/**
+ * The ApolloProvider component wraps your React app and places the client on the context, which allows you to access it from anywhere in your component tree.
+ */
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<Pages />
+	</ApolloProvider>,
+	document.getElementById('root')
+)
+
+// // Apollo tracks loading state and networkStatus of query automatically
+// client
+// 	.query({
+// 		query: gql`
+// 			query GetLaunch {
+// 				launch(id: 56) {
+// 					id
+// 					mission {
+// 						name
+// 					}
+// 				}
+// 			}
+// 		`,
+// 	})
+// 	.then(console.log)
